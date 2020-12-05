b14016283edcb314d22ee3c966ab8b542621e1b7
varun <varunprakash652@gmail.com>
2020-12-06 04:59:21 +0530

    Db commit

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users.js');
 const profile = require('./routes/api/profile.js');
 const posts = require('./routes/api/posts.js');
 
 const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); 
+
 
 // Database configuration
 
